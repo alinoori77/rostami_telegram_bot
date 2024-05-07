873bcf4a5aac068260534a1a3445ec66e1630400
Ali Noori <125356047+alinoori77@users.noreply.github.com>
2024-05-07 08:07:58 +0000

    add config sample file

---
+++ config_sample.py
@@ -0,0 +1,13 @@
+import os
+
+api_id = 
+api_hash = ""
+bot_token = ""
+
+Base_dir = os.path.dirname(__file__)
+
+font_path = Base_dir + "/B Zar Bold.ttf"
+watermark= Base_dir + "/watermark.png"
+gradient= Base_dir + "/gradient.png"
+
+valid_user = ['']
